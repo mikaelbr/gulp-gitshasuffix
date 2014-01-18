afdea860e848f24ff7a7f1fa95434cd7f0eb449a
Mikael Brevik <mikaelbre@gmail.com>
2014-01-18 16:32:33 +0100

    Adds tests

--- test/git.js
+++ test/git.js
@@ -18,7 +18,7 @@ describe("gulp-gitshasuffix", function () {
     git.exec = function (app, args, extra, cb) {
       ++fnCalls;
       app.should.equal("git");
-      cb(null, "");
+      cb(null, "fbb790d601f7cb0ad0fabe5feff023b02aa9a03d");
     };
     git.getLatestSha(function (err) {
       should(err).equal(null);
@@ -27,6 +27,46 @@ describe("gulp-gitshasuffix", function () {
     });
   });
 
-  // More tests to come.
+  it("should give error if git is not found on the system", function (done) {
+    git.which = function (app, cb) {
+      cb(new Error());
+    };
+    git.exec = function (app, args, extra, cb) {
+      cb(null, "");
+    };
+    git.getLatestSha(function (err, data) {
+      should(err.message).equal("git not found on your system.");
+      done();
+    });
+  });
+
+  it("should give error if returned sha is malformed", function (done) {
+    git.which = function (app, cb) {
+      cb();
+    };
+    git.exec = function (app, args, extra, cb) {
+      cb(null, "SOMETHING NOT A SHA");
+    };
+    git.getLatestSha(function (err, data) {
+      should.exist(err);
+      err.message.should.equal("Malformed commit sha or no sha at all returned.");
+      done();
+    });
+  });
+
+  it("should return commit sha if it matches", function (done) {
+    git.which = function (app, cb) {
+      cb();
+    };
+    git.exec = function (app, args, extra, cb) {
+      cb(null, "fbb790d601f7cb0ad0fabe5feff023b02aa9a03d\n");
+    };
+    git.getLatestSha(function (err, data) {
+      should.not.exist(err);
+      should.exist(data);
+      data.should.equal("fbb790d601f7cb0ad0fabe5feff023b02aa9a03d");
+      done();
+    });
+  });
 
 });
--- test/main.js
+++ test/main.js
@@ -3,7 +3,8 @@
 
 var should = require("should"),
   through = require("through2"),
-  join = require("path").join,
+  path = require("path"),
+  join = path.join,
   git = require("../lib/git");
 
 require("mocha");
@@ -21,5 +22,124 @@ describe("gulp-gitmodified", function () {
     done();
   });
 
+  it('should suffix folders', function(done) {
+    var testSha = "aaabbbcccddd0123456",
+        instream = gulp.src(join(__dirname, "./fixtures")),
+        outstream = suffix();
 
+    git.getLatestSha = function (cb) {
+      return cb(null, testSha);
+    };
+
+    outstream.on('data', function(file) {
+      should.exist(file);
+      should.exist(file.path);
+      should.exist(file.relative.indexOf("aaabb") !== -1);
+      done();
+    });
+
+    instream.pipe(outstream);
+  });
+
+  it('should default to - as seperator and the first 6 characters', function(done) {
+    var testSha = "fbb790d601f7cb0ad0fabe5feff023b02aa9a03d",
+        instream = gulp.src(join(__dirname, "./fixtures")),
+        outstream = suffix();
+
+    git.getLatestSha = function (cb) {
+      return cb(null, testSha);
+    };
+
+    outstream.on('data', function(file) {
+      var split = file.relative.split("-");
+      should.exist(file);
+      should.exist(file.path);
+      should.exist(split);
+      should.exist(split[1]);
+      split.length.should.equal(2);
+      split[1].should.equal(testSha.substring(0, 6));
+      done();
+    });
+
+    instream.pipe(outstream);
+  });
+
+  it('should take suffix length as argument', function(done) {
+    var testSha = "fbb790d601f7cb0ad0fabe5feff023b02aa9a03d",
+        length = 8,
+        instream = gulp.src(join(__dirname, "./fixtures")),
+        outstream = suffix(length);
+
+    git.getLatestSha = function (cb) {
+      return cb(null, testSha);
+    };
+
+    outstream.on('data', function(file) {
+      var split = file.relative.split("-");
+      should.exist(file);
+      should.exist(file.path);
+      should.exist(split);
+      should.exist(split[1]);
+      split.length.should.equal(2);
+      split[1].length.should.equal(length);
+      split[1].should.equal(testSha.substring(0, length));
+      done();
+    });
+
+    instream.pipe(outstream);
+  });
+
+  it('should take seperator as argument', function(done) {
+    var testSha = "fbb790d601f7cb0ad0fabe5feff023b02aa9a03d",
+        seperator = "***",
+        instream = gulp.src(join(__dirname, "./fixtures")),
+        outstream = suffix(6, seperator);
+
+    git.getLatestSha = function (cb) {
+      return cb(null, testSha);
+    };
+
+    outstream.on('data', function(file) {
+      should.exist(file.relative.indexOf(seperator) !== -1);
+      var split = file.relative.split(seperator);
+      should.exist(file);
+      should.exist(file.path);
+      should.exist(split);
+      should.exist(split[1]);
+      split.length.should.equal(2);
+      split[1].should.equal(testSha.substring(0, 6));
+      done();
+    });
+
+    instream.pipe(outstream);
+  });
+
+  it('should set suffix on all files', function(done) {
+    var testSha = "fbb790d601f7cb0ad0fabe5feff023b02aa9a03d",
+        seperator = "***",
+        instream = gulp.src(join(__dirname, "./fixtures/*")),
+        outstream = suffix(6, seperator);
+
+    git.getLatestSha = function (cb) {
+      return cb(null, testSha);
+    };
+
+    outstream.on('data', function(file) {
+      var ext = path.extname(file.path);
+      should.exist(file.relative.indexOf(seperator) !== -1);
+      var split = file.relative.split(seperator);
+      should.exist(file);
+      should.exist(file.path);
+      should.exist(split);
+      should.exist(split[1]);
+      split.length.should.equal(2);
+      split[1].should.equal(testSha.substring(0, 6) + ext);
+    });
+
+    outstream.on('end', function () {
+      done();
+    });
+
+    instream.pipe(outstream);
+  });
 });
