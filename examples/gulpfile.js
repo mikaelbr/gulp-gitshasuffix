33635ea7a9009fa3a507dca7a1e9d96cdd860869
Mikael Brevik <mikaelbre@gmail.com>
2014-05-29 00:26:13 +0200

    Adds additional examples to the gulpfile

--- examples/gulpfile.js
+++ examples/gulpfile.js
@@ -2,15 +2,28 @@ var gulp = require("gulp"),
     through = require('through2'),
     suffix = require("../");
 
+var wrap = function (stream) {
+  stream.on('error', function (err) {
+    console.log(err);
+  })
+  .pipe(through.obj(function (file, enc, cb) {
+    console.log("New path: ", file.path);
+    this.push(file);
+    return cb()
+  }));
+}
+
 gulp.task("default", function () {
-  var data = gulp.src(["../test/fixtures/*"])
-    .pipe(suffix())
-    .on('error', function (err) {
-      console.log(err);
-    })
-    .pipe(through.obj(function (file, enc, cb) {
-      console.log("New path: ", file.path);
-      this.push(file);
-      return cb()
-    }));
+  wrap(gulp.src(["../test/fixtures/*"])
+    .pipe(suffix()))
+});
+
+gulp.task("folder", function () {
+  wrap(gulp.src(["../test/fixtures"])
+    .pipe(suffix()))
+});
+
+gulp.task("folderPrefix", function () {
+  wrap(gulp.src(["../test/fixtures/*"])
+    .pipe(suffix({ folder: true })))
 });
